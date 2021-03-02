07f8fd44a76790f974d91ad8d51d3663f5ad5c51
Calum <cjb240801@gmail.com>
2021-03-02 17:55:43 +0000

    New function update heading and paragraph on click

--- Week-6/index.js
+++ Week-6/index.js
@@ -7,3 +7,13 @@ function print5(){
 }
 print5();
 print5();
+
+function updateAfterLoad(){
+    let heading = document.getElementById('heading');
+    let paragraph = document.getElementById('paragraph');
+    heading.firstChild.nodeValue = 'Updated heading';
+    paragraph.firstChild.nodeValue = 'Updated paragraph';
+}
+
+document.addEventListener('click', updateAfterLoad);
+
