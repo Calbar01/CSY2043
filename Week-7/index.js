679470a27aaf0b337f2bed419b63ae6efe0b1d71
Calum <cjb240801@gmail.com>
2021-03-09 16:59:56 +0000

    second exercise complete

--- Week-7/index.js
+++ Week-7/index.js
@@ -1,6 +1,9 @@
 //First exercise to popup an alert when circle pressed
+//Second exercise to change background color to blue on click
 function clickEvent(){
     alert('Circle was pressed');
+    let circle = document.getElementById('circle');
+    circle.style.backgroundColor = 'blue';
 }
 
 function changeCircle(){
