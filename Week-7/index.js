b3b2d01dd51d39bbaea4a1c3f9e24a321c933cab
Calum <cjb240801@gmail.com>
2021-03-09 17:13:10 +0000

    fourth exercise

--- Week-7/index.js
+++ Week-7/index.js
@@ -1,19 +1,17 @@
 //First exercise to popup an alert when circle pressed
 //Second exercise to change background color to blue on click
 //Third exercise to set opacity to 0.5
+//Fourth exercise to nset opacity to 1 from the css 0.5
 function clickEvent(){
     //alert('Circle was pressed');
     let circle = document.getElementById('circle');
     circle.style.backgroundColor = 'blue';
-    circle.style.opacity = 0.5;
+    circle.style.opacity = 1;
+    circle.style.transitionDuration = '2s';
 }
 
 function changeCircle(){
     let circle = document.getElementById('circle');
     circle.addEventListener('click', clickEvent);
 }
-
-
-
-
 document.addEventListener('DOMContentLoaded', changeCircle);
