4c64e2199b2453f255066927ef4dcd23068a31a2
Calum <cjb240801@gmail.com>
2021-03-16 18:00:43 +0000

    configuring the charts

--- Week-8/index.js
+++ Week-8/index.js
@@ -1,5 +1,34 @@
-let ctx = document.getElementById('myChart').getContext('2d');
-let chart = new Chart(ctx, {
+// let element = document.getElementById('myChart'); //.getContext('2d');
+
+// // The type of chart we want to create
+// let chartType = "line";
+
+// // The data for our dataset
+// let chartData = {
+//         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+//         datasets: [ 
+//             {
+//             label: 'My First dataset',
+//             backgroundColor: 'rgb(255, 99, 132)',
+//             borderColor: 'rgb(255, 99, 132)',
+//             data: [0, 10, 5, 2, 20, 30, 45]
+//             }
+//         ]
+//     };
+
+// // Configuration options go here
+// let chartOptions = {};
+
+// let param = {
+//     type: chartType,
+//     data: chartData,
+//     options: chartOptions
+// };
+
+// let chart = new chart(element, param);
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
     // The type of chart we want to create
     type: 'line',
 
@@ -8,8 +37,8 @@ let chart = new Chart(ctx, {
         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
         datasets: [{
             label: 'My First dataset',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'rgb(20, 99, 132)',
+            borderColor: 'rgb(20, 99, 132)',
             data: [0, 10, 5, 2, 20, 30, 45]
         }]
     },
@@ -17,3 +46,5 @@ let chart = new Chart(ctx, {
     // Configuration options go here
     options: {}
 });
+
+
